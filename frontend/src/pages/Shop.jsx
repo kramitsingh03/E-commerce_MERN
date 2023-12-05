a27615cffc280b2b4e99d570899dd8e8afb98f1b
Amit <amitkumar03022004@gmail.com>
2023-12-06 02:48:15 +0530

    add to cart

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
+import Popular from '../components/Popular/Popular'
 
 const Shop = () => {
     return (
         <div><Hero />
-            
+            <Popular/>
         </div>
     )
 }
